dbb7db64ee11de2eec5cc23e588c96f1988e210f
Muhammad Mufid Irfan  Farras <m.mufid1804@gmail.com>
2023-08-27 13:31:05 +0000

    Develop HTML structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import TodoItem from "$lib/todo-item.svelte";
+
+    const title = "To-dos";
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+
+<!-- Input form -->
+<form action="" method="">
+    <input type="text" name="text" aria-label="Add a to-dos" placeholder="+ type to add a to-do">
+</form>
+
+<!-- To-do lists -->
+<TodoItem />
+<TodoItem />
+<TodoItem />
+
+
